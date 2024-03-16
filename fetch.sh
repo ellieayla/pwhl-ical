29a0639903951f3ae5a8238627bfb52ea4ad19b6
ellieayla <1447600+me@users.noreply.github.com>
2024-03-15 22:27:10 -0400

    Seperate feed per season

--- fetch.sh
+++ fetch.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-echo "BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//Hockeytech//Leaguestat iCal//EN\nBEGIN:VCALENDAR";
-curl -s $(curl -s https://lscluster.hockeytech.com/feed/index.php\?feed\=statviewfeed\&view\=schedule\&team\=-1\&season\=1\&month\=3\&location\=homeaway\&key\=694cfeed58c932ee\&client_code\=pwhl\&site_id\=2\&league_id\=1\&division_id\=-1\&lang\=en | jq -R -s -r '.[1+index("("): rindex(")")] | fromjson | .[0].sections[0].data | .[] | select(.prop.mobile_calendar) | .prop.mobile_calendar.link') | awk -v FS=: -v OFS=: '/^BEGIN:VEVENT/{flag=1};$1 == "DESCRIPTION" {$2="https://www.youtube.com/@thepwhlofficial/streams"}; flag;/^END:VEVENT/{flag=0}'
-echo "END:VCALENDAR"
+for SEASON in $(curl -s 'https://lscluster.hockeytech.com/feed/index.php?feed=statviewfeed&view=bootstrap&season=1&game_id=&pageName=schedule&key=694cfeed58c932ee&client_code=pwhl&site_id=2&league_id=1&league_code=&lang=en' | jq -R -s -r '.[1+index("("): rindex(")")] | fromjson | .seasons[].id');
+
+do
+ URL="https://lscluster.hockeytech.com/feed/index.php?feed=statviewfeed&view=schedule&team=-1&season=${SEASON}&month=-1&location=homeaway&key=694cfeed58c932ee&client_code=pwhl&site_id=2&league_id=1&division_id=-1&lang=en"
+ echo "fetching $URL"
+  (echo "BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//Hockeytech//Leaguestat iCal//EN\nMETHOD:PUBLISH";
+   curl -s $(curl -s "$URL" | jq -R -s -r '.[1+index("("): rindex(")")] | fromjson | .[0].sections[0].data | .[] | select(.prop.mobile_calendar) | .prop.mobile_calendar.link') | awk -v FS=: -v OFS=: '/^BEGIN:VEVENT/{flag=1};$1 == "DESCRIPTION" {$2="https://www.youtube.com/@thepwhlofficial/streams"}; flag;/^END:VEVENT/{flag=0}'
+   echo "END:VCALENDAR") > season-$SEASON.ical
+done
