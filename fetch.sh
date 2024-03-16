6da0df2a39e754f4c172923809712f82a57fa18e
ellieayla <1447600+me@users.noreply.github.com>
2024-03-15 22:46:09 -0400

    ICS files are -r-n

--- fetch.sh
+++ fetch.sh
@@ -5,7 +5,5 @@ for SEASON in $(curl -s 'https://lscluster.hockeytech.com/feed/index.php?feed=st
 do
  URL="https://lscluster.hockeytech.com/feed/index.php?feed=statviewfeed&view=schedule&team=-1&season=${SEASON}&month=-1&location=homeaway&key=694cfeed58c932ee&client_code=pwhl&site_id=2&league_id=1&division_id=-1&lang=en"
  echo "fetching $URL"
-  (echo "BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//Hockeytech//Leaguestat iCal//EN\nMETHOD:PUBLISH";
-   curl -s $(curl -s "$URL" | jq -R -s -r '.[1+index("("): rindex(")")] | fromjson | .[0].sections[0].data | .[] | select(.prop.mobile_calendar) | .prop.mobile_calendar.link') | awk -v FS=: -v OFS=: '/^BEGIN:VEVENT/{flag=1};$1 == "DESCRIPTION" {$2="https://www.youtube.com/@thepwhlofficial/streams"}; flag;/^END:VEVENT/{flag=0}'
-   echo "END:VCALENDAR") > season-$SEASON.ical
+ curl -s $(curl -s "$URL" | jq -R -s -r '.[1+index("("): rindex(")")] | fromjson | .[0].sections[0].data | .[] | select(.prop.mobile_calendar) | .prop.mobile_calendar.link') | awk -v ORS="\r\n" -v FS=: -v OFS=: 'BEGIN { printf "BEGIN:VCALENDAR\r\nVERSION:2.0\r\nPRODID:-//Hockeytech//Leaguestat iCal//EN\r\nMETHOD:PUBLISH\r\n";} END {printf "END:VCALENDAR\r\n";} /^BEGIN:VEVENT/{flag=1};$1 == "DESCRIPTION" {$2="https://www.youtube.com/@thepwhlofficial/streams"}; flag;/^END:VEVENT/{flag=0}' > season-$SEASON.ical
 done
