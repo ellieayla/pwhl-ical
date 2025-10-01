4efc397035a3fe6a3275f0edc2743cf8d098840f
ellieayla <1447600+me@users.noreply.github.com>
2025-10-01 19:22:18 -0400

    numeric comparison using -gt

--- update-readme.sh
+++ update-readme.sh
@@ -5,8 +5,8 @@ cp README.tmpl README.md
 
 for filename in `ls -r season-*.ical`
 do
-	ROWS=$(cat $filename | wc -l)
-	if (( ROWS > 5 )); then
+	export ROWS=$(echo $(cat $filename | wc -l))
+	if [[ "$ROWS" -gt "5" ]]; then
 		echo " * [$filename](webcal://github.com/$GITHUB_REPOSITORY/raw/refs/heads/main/$filename)" >> README.md
 	else
 		echo " * $filename finished." >> README.md
