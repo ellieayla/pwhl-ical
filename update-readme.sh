765aaf5465bd04a4eb1a968a2906027b7d6fce92
ellieayla <1447600+me@users.noreply.github.com>
2025-10-01 19:26:21 -0400

    fix

--- update-readme.sh
+++ update-readme.sh
@@ -7,7 +7,7 @@ for filename in `ls -r season-*.ical`
 do
 	export ROWS=$(echo $(cat $filename | wc -l))
 	if [ "$ROWS" -gt "5" ]; then
-		echo " * [$filename](webcal://github.com/$GITHUB_REPOSITORY/raw/refs/heads/main/$filename)" >> README.md
+		echo " * [$filename](https://github.com/$GITHUB_REPOSITORY/raw/refs/heads/main/$filename)" >> README.md
 	else
 		echo " * $filename finished." >> README.md
 	fi
