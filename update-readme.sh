57bbe97db8b8ab13bf6b134c3d08b527e65debd5
ellieayla <1447600+me@users.noreply.github.com>
2025-10-01 19:23:30 -0400

    one bracket

--- update-readme.sh
+++ update-readme.sh
@@ -6,7 +6,7 @@ cp README.tmpl README.md
 for filename in `ls -r season-*.ical`
 do
 	export ROWS=$(echo $(cat $filename | wc -l))
-	if [[ "$ROWS" -gt "5" ]]; then
+	if [ "$ROWS" -gt "5" ]; then
 		echo " * [$filename](webcal://github.com/$GITHUB_REPOSITORY/raw/refs/heads/main/$filename)" >> README.md
 	else
 		echo " * $filename finished." >> README.md
